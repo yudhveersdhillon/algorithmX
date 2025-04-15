04a1cd4045491c56191ca1c1465837321ce3f84b
yudhveerdhillon <yudhveersinghvision@gmail.com>
2025-04-15 23:36:44 +0530

    initial Commitss

--- controller/userController.js
+++ controller/userController.js
@@ -5,7 +5,7 @@ const Task = require("../models/task");
 
 const Util = require("../utils/commonUtils");
 
-const SECRET_KEY = process.env.JWT_SECRET || "yourSecretKey"; // use env var in production
+const SECRET_KEY = process.env.JWT_SECRET || "yourSecretKey"; 
 
 exports.register = async (req, res) => {
   try {
@@ -15,14 +15,11 @@ exports.register = async (req, res) => {
       } else if (password && !Util.isValidPassword(password)) {
         return res.status(400).json({ message: "Invaid password format"});
       }
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-    // Save new user
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
@@ -37,20 +34,19 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Find user
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    // Check password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Create token
     const token = jwt.sign({ userId: user._id, email: user.email }, SECRET_KEY, { expiresIn: "7d" });
+    user.token = token;
 
+    user.password = undefined;
     return res.status(200).json({ message: "Login successful", user });
   } catch (err) {
     console.error("Login error:", err);
@@ -79,8 +75,8 @@ exports.taskCreate = async (req, res) => {
 
   exports.taskUpdate = async (req, res) => {
     try {
-      const { taskId } = req.params;  // Get task ID from the route parameter
-      const updatedData = req.body;  // Get updated task data from the body
+      const { taskId } = req.params;  
+      const updatedData = req.body;  
   
       const updatedTask = await Task.findByIdAndUpdate(taskId, updatedData, { new: true });
       
